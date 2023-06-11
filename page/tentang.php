8f2fbea626dbe2c42bb9cbb93223312715fe8575
provo <sofyan.saputra@students.amikom.ac.id>
2023-06-11 12:35:10 +0700

    First Commit

---
+++ page/tentang.php
@@ -0,0 +1,58 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil Pembuat</b></p>
+</div>
+<div id="box">
+
+  <h1>Tentang</h1>
+  <img src="img/pace.png" width="150" align="left"/>
+  <table>
+    <tr>
+      <ul>
+        <td width="25%">
+          <li><b>NAMA</b></li>
+        </td>
+        <td>: Fuadi Mukhlis</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>NIM</b></li>
+        </td>
+        <td>: 20.11.3453</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Jurusan</b></li>
+        </td>
+        <td>: S1 Informatika</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Hobby</b></li>
+        </td>
+        <td>: Ngoding, Belajar bahasa pemrograman baru, Futsal, Berenang, dll</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Minat</b></li>
+        </td>
+        <td>: Multimedia & Design</td>
+      </ul>
+    </tr>
+
+   
+
+  </table>
+
+</div>
